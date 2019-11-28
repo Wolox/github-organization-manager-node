7d967ac6e02fd4b38485f752dee64a673eda1f4d
Federico Casares <federico.casares@wolox.com.ar>
2019-11-28 10:45:07 -0300

    Auth checks

--- app/middlewares/checkScope.js
+++
@@ -1,5 +0,0 @@
-const jwtAuthz = require('express-jwt-authz');
-
-exports.adminScope = jwtAuthz(['read:messages']);
-exports.techLeaderScope = jwtAuthz(['read:messages']);
-exports.devScope = jwtAuthz(['read:messages']);
--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,4 @@
+const jwtAuthz = require('express-jwt-authz');
 const {
   createRepository,
   addTeamToRepo,
@@ -10,45 +11,25 @@ const { getTeams, createTeam, addMembersToTeam, deleteTeam } = require('./contro
 const { healthCheck } = require('./controllers/healthCheck');
 const { getUsersHandler, setUserMaintainerHandler, setUserAdminHandler } = require('./controllers/users.js');
 const checkJwt = require('./middlewares/checkAuth');
-const { adminScope } = require('./middlewares/checkScope');
 
 exports.init = app => {
   app.get('/health', healthCheck);
 
-  app.get('/search/repositories', searchRepositories);
-  app.get('/repositories', getRepositories);
-  app.post('/repositories', createRepository);
-  app.post('/organization/:username', addUserToOrganization);
+  app.get('/search/repositories', checkJwt, searchRepositories);
+  app.get('/repositories', checkJwt, getRepositories);
 
-  app.post('/repositories/:repoName/teams', addTeamToRepo);
+  app.post('/repositories', checkJwt, jwtAuthz(['create:repo']), createRepository);
+  app.post('/repositories/:repoName/teams', checkJwt, jwtAuthz(['create:repo']), addTeamToRepo);
+  app.post('/repositories/:repoName/codeowners', checkJwt, jwtAuthz(['create:repo']), addCodeownersToRepo);
 
-  app.post('/repositories/:repoName/codeowners', addCodeownersToRepo);
+  app.post('/organization/:username', checkJwt, jwtAuthz(['add:user:org']), addUserToOrganization);
 
-  app.get('/teams', getTeams);
-  app.post('/teams', createTeam);
-  app.post('/teams/:teamId/members', addMembersToTeam);
-  app.delete('/teams/:teamId', deleteTeam);
+  app.get('/teams', checkJwt, jwtAuthz(['manage:teams']), getTeams);
+  app.post('/teams', checkJwt, jwtAuthz(['manage:teams']), createTeam);
+  app.post('/teams/:teamId/members', checkJwt, jwtAuthz(['manage:teams']), addMembersToTeam);
+  app.delete('/teams/:teamId', checkJwt, jwtAuthz(['manage:teams']), deleteTeam);
 
-  app.get('/users', checkJwt, getUsersHandler);
-  app.post('/users/:userId/maintainer', checkJwt, setUserMaintainerHandler);
-  app.post('/users/:userId/admin', checkJwt, setUserAdminHandler);
-
-  app.get('/api/public', (req, res) => {
-    res.json({
-      message: "Hello from a public endpoint! You don't need to be authenticated to see this."
-    });
-  });
-
-  app.get('/api/private', checkJwt, (req, res) => {
-    res.json({
-      message: 'Hello from a private endpoint! You need to be authenticated to see this.'
-    });
-  });
-
-  app.get('/api/private-scoped', checkJwt, adminScope, (req, res) => {
-    res.json({
-      message:
-        'Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this.'
-    });
-  });
+  app.get('/users', checkJwt, jwtAuthz(['add:role']), getUsersHandler);
+  app.post('/users/:userId/techlead', checkJwt, jwtAuthz(['add:role']), setUserMaintainerHandler);
+  app.post('/users/:userId/admin', checkJwt, jwtAuthz(['add:role']), setUserAdminHandler);
 };
