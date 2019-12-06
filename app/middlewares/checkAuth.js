96ca4a147b33673b8a0f8017e7c1eb4ea78aa201
Federico Casares <federico.casares@wolox.com.ar>
2019-12-06 16:12:41 -0300

    Fix audience innecesary check

--- app/middlewares/checkAuth.js
+++ app/middlewares/checkAuth.js
@@ -10,9 +10,6 @@ module.exports = jwt({
     jwksRequestsPerMinute: 5,
     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
-
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256']
 });
