ba060e079daa6980516c61f0d8a8328f149c9840
Rodrigo Videla <rodrigo.videla@wolox.com.ar>
2020-01-30 16:32:17 -0300

    Small change of the code formatting.

--- app/controllers/organization.js
+++ app/controllers/organization.js
@@ -2,6 +2,4 @@ const { addUser } = require('../services/github/organization');
 
 const addUserToOrganization = (req, res) => addUser(req.params.username).then(resp => res.send(resp));
 
-module.exports = {
-  addUserToOrganization
-};
+module.exports = { addUserToOrganization };
